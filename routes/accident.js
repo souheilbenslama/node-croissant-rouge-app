c20caa868211e8bf6161bea9d20e35f2230160ad
wafaselmi <57751784+wafaselmi@users.noreply.github.com>
2021-03-21 20:58:16 +0100

    setting status to finished automatically

--- routes/accident.js
+++ routes/accident.js
@@ -24,8 +24,43 @@ router.put('/finished/:id' , async(req, res) => {
     catch (ex){
         res.send(ex);
     }
-}
-    )
+})
+//creating alerte and changing Accident's status to finished automatically after 1 hour
+router.post('/', async (req, res) => {
+    
+    const {id_temoin,
+        location,
+        protectionDesc,
+        hemorragieDesc,
+        respirationDesc,conscienceDesc } = req.body ; 
+
+        const accident = new Accident({
+            id_temoin: id_temoin,
+            location: location,
+            protectionDesc: protectionDesc,
+            hemorragieDesc: hemorragieDesc,
+            respirationDesc: respirationDesc,
+            conscienceDesc: conscienceDesc,
+        });
+  
+         console.log(accident) ; 
+
+    try {
+       
+        var result = await accident.save() ; 
+        res.send(result);
+        setTimeout(function(){
+            accident.update({$set: {"status": "Finished"}});
+
+        },3600000)
+
+    } catch (ex) {
+        res.send(ex);
+    }
+
+})
+
+
 
 
 
